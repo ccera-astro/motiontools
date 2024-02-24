3c11e495735f257e9ed323f5111342be1421edd8
Marcus D. Leech <patchvonbraun@gmail.com>
2024-02-24 15:00:09 -0500

    Added "query_rate" code, so that watchdog can take action if the axis is   apparently running too fast.

--- SensorServer.py
+++ SensorServer.py
@@ -13,6 +13,10 @@ import threading
 
 current_elev = 0.0
 current_az = 0.0
+last_elev = -99.00
+last_az = -99.00
+elev_rate = 0.0
+az_rate = 0.0
 
 elSensor = None
 azSensor = None
@@ -59,20 +63,57 @@ def get_az_sensor():
 def query_el_sensor():
     global current_elev
     return (current_elev)
+    
+def query_el_rate():
+    global elev_rate
+    return (elev_rate)
 
 def query_az_sensor():
     global current_az
     return (current_az)
 
+def query_az_rate():
+    global az_rate
+    return (az_rate)
+
 def device_loop():
-	global current_az
-	global current_elev
-	
-	while (True):
-		current_az = get_az_sensor()
-		time.sleep(0.05)
-		current_elev = get_el_sensor()
-		time.sleep(0.100)
+    global current_az
+    global current_elev
+    
+    while (True):
+        current_az = get_az_sensor()
+        if (last_az < -20.0):
+            last_az = current_az
+            last_az_time = time.time()
+        time.sleep(0.05)
+
+        current_elev = get_el_sensor()
+        if (last_elev < -20.0):
+            last_elev = current_elev
+            last_elev_time = time.time()
+        
+        #
+        # Compute rates
+        #
+        #
+        # Elevation
+        t = current_elev - last_elev
+        elev_rate = abs(t) / (time.time() - last_elev_time())
+        
+        #
+        # Azimuth
+        #
+        t = current_az - last_az
+        az_rate = abs(t) / (time.time() - last_az_time())
+        
+        #
+        # Once computed, make "last" == "current"
+        #
+        last_elev = current_elev
+        last_elev_time = time.time()
+        last_az = current_az
+        last_az_time = time.time()
+        time.sleep(0.250)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--xmlport", type=int, required=True, help="XML Port")
@@ -87,6 +128,8 @@ SENSORS_PORT = args.serialport
 xmlserver = SimpleXMLRPCServer(('0.0.0.0', args.xmlport), allow_none=True, logRequests=False)
 xmlserver.register_function(query_el_sensor)
 xmlserver.register_function(query_az_sensor)
+xmlserver.register_function(query_az_rate)
+xmlserver.register_function(query_el_rate)
 server_thread = threading.Thread(target=xmlserver.serve_forever)
 server_thread.daemon = True
 server_thread.start()
