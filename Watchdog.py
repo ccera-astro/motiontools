64c4726e49acb5d8384c6a54a15aa778b46a1027
Marcus D. Leech <patchvonbraun@gmail.com>
2024-02-26 15:02:22 -0500

    Have watchdog pay attention to axis speed.

--- Watchdog.py
+++ Watchdog.py
@@ -9,16 +9,26 @@ def dissect(s):
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--proxy", type=str, default="http://localhost:36036", help="XMLRPC Server")
+parser.add_argument("--sensorproxy", type=str, default="http://localhost:9090", help="XMLRPC Sensor Server")
 parser.add_argument("--timeout", type=int, default=900, help="Watchdog timeout (seconds)")
 args = parser.parse_args()
 rpc = xml.ServerProxy(args.proxy)
+rpc2 = xml.ServerProxy(args.sensorproxy)
 stamp = 0
 OKrets = ["None", "BADNODE"]
-sleeptime = 10
+sleeptime = 3
 while True:
     now = time.time()
     try:
         stamp = rpc.QueryTime(0)
+        elspeed = rpc2.query_el_rate()
+        azspeed = rpc2.query_az_rate()
+        if (elspeed > 0.6 or azspeed > 1.5):
+            rpc.Shutdown(0)
+            time.sleep(5)
+            rpc.Shutdown(1)
+            time.sleep(1)
+            rpc.SysExit(0)
         alerts_0 = rpc.QueryAlerts(0)
         alerts_1 = rpc.QueryAlerts(1)
         if (alerts_0 not in OKrets):
