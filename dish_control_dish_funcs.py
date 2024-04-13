4bd57ef493d86d1210fa7e6d56432dd446cc61b8
Marcus D. Leech <patchvonbraun@gmail.com>
2024-04-13 11:31:18 -0400

    Added "Exit Server" and "Reset Limits" buttons to dish_control.grc

--- dish_control_dish_funcs.py
+++ dish_control_dish_funcs.py
@@ -4,6 +4,7 @@ import numpy
 import os
 import sys
 import xmlrpc.client as xml
+import time
 
 RPC = None
 RPC2 = None
@@ -16,12 +17,41 @@ def init_RPC(srvr):
         except:
             RPC = None
 
+def sysExit(v,srvr):
+    global RPC
+    init_RPC(srvr)
+    
+    if (v == 1):
+        try:
+            RPC.SysExit(0)
+        except:
+            pass
+    
 def init_RPC2(srvr):
     global RPC2
     if (RPC2 == None):
         RPC2 = xml.ServerProxy(srvr)
-    
+
+def ResetLimits(btn,srvr):
+    if (btn == 1):
+        axLimits(0,srvr)
+        time.sleep(0.25)
+        axLimits(1,srvr)
+       
+def axLimits(ax,srvr):
+    global RPC
+    init_RPC(srvr)
+    try:
+        RPC.AccLimit(ax, 1000)
+        time.sleep(0.25)
+        RPC.VelLimit(ax, 1200)
+        rv = True
+    except:
+        rv = False
+    return rv
+
 def tryMove(lbl, ax,spd):
+    global RPC
     try:
         RPC.Move(ax,spd)
     except:
@@ -86,6 +116,6 @@ def get_posns(pacer,srvr):
     try:
         posns = RPC2.query_both_axes()
     except:
-        print ("get_posns called, but exception was raised")
+        #print ("get_posns called, but exception was raised")
         posns = (-1000, -1000)
     return(posns)
