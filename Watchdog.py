e949cef127508adcc22b9d6c7d4e9cbd6770d271
Marcus D. Leech <patchvonbraun@gmail.com>
2024-04-16 22:05:02 -0400

    Adding "token" stuff to watchdog.py

--- Watchdog.py
+++ Watchdog.py
@@ -2,7 +2,43 @@
 import time
 import xmlrpc.client as xml
 import argparse
+import xmlrpc.server
+from xmlrpc.server import SimpleXMLRPCServer
+apps = {}
+def register_app(atoken):
+    global apps
+    apps[atoken] = time.time()
 
+def deregister_app(atoken):
+    global apps
+    if (atoken in apps):
+        del apps[atoken]
+
+def update_activity(atoken):
+    global apps
+    if (atoken in apps):
+        apps[atoken] = time.time()
+
+def wdogloop():
+    global apps
+    global rpc
+    
+    while True:
+        apptime = time.time()
+        for a in apps:
+            if ((apptime - apps[a]) > 10):
+                print ("App %s has triggered a watchdog--setting motors to zero speed" % a)
+                #
+                # Do a thing
+                #
+                try:
+                    rpc.Move(0,0.0)
+                    time.sleep(0.5)
+                    rpc.Move(1,0.0)
+                except:
+                    pass
+        time.sleep(0.5)
+        
 def dissect(s):
     lines = s.split("\n")
     return(lines[1])
@@ -11,19 +47,47 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--proxy", type=str, default="http://localhost:36036", help="XMLRPC Server")
 parser.add_argument("--sensorproxy", type=str, default="http://localhost:9090", help="XMLRPC Sensor Server")
 parser.add_argument("--timeout", type=int, default=900, help="Watchdog timeout (seconds)")
+parser.add_argument("--myport", type=int, default=6660, help="My XMLRPC port")
+
+#
+# Create and run the XML server in a separate thread
+#
+xmlserver = SimpleXMLRPCServer(('0.0.0.0', args.myport), allow_none=True, logRequests=False)
+xmlserver.register_function(register_app)
+xmlserver.register_function(update_activity)
+xmlserver.register_function(deregister_app)
+server_thread = threading.Thread(target=xmlserver.serve_forever)
+server_thread.daemon = True
+server_thread.start()
+
 args = parser.parse_args()
 rpc = xml.ServerProxy(args.proxy)
 rpc2 = xml.ServerProxy(args.sensorproxy)
 stamp = 0
 OKrets = ["None", "BADNODE"]
 sleeptime = 3
+
+#
+# Create and run the XML server in a separate thread
+#
+xmlserver = SimpleXMLRPCServer(('0.0.0.0', args.myport), allow_none=True, logRequests=False)
+xmlserver.register_function(register_app)
+xmlserver.register_function(update_activity)
+server_thread = threading.Thread(target=xmlserver.serve_forever)
+server_thread.daemon = True
+server_thread.start()
+
+wdog_thread = threading.Thread(target=wdogloop)
+wdog_thread.daemon = True
+wdog_thread.start()
+
 while True:
     now = time.time()
     try:
         stamp = rpc.QueryTime(0)
         elspeed = rpc2.query_el_rate()
         azspeed = rpc2.query_az_rate()
-        if (elspeed > 0.3 or azspeed > 0.5):
+        if (elspeed > 0.25 or azspeed > 0.5):
             rpc.Shutdown(0)
             time.sleep(1)
             rpc.Shutdown(1)
@@ -48,6 +112,7 @@ while True:
         time.sleep (sleeptime)
         if (sleeptime < 60):
             sleeptime *= 2
-    time.sleep(5)
+            
+        
 
         
