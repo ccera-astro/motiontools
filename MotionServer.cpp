453dd6ea965fb4ad73887161d4847d3c510e2859
Marcus D. Leech <patchvonbraun@gmail.com>
2024-06-10 13:40:05 -0400

    More diagnostics.

--- MotionServer.cpp
+++ MotionServer.cpp
@@ -889,7 +889,7 @@ public:
         paramList.verifyEnd(1);
         
         time(&heartbeat);
-        fprintf (stderr, "Heatbeating at %ld", (long int)heartbeat);
+        fprintf (stderr, "Heatbeating at %ld\n", (long int)heartbeat);
         *retvalP = xmlrpc_c::value_int(0);
     }
 };
